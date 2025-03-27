5eb6dc28d6a9a62b965d88eaf45efe05c0f4c950
Quincy <quincy@games.com>
2025-03-27 12:08:26 +0800

    refactor(components): 优化文章时间显示
    - 在 PostContent 组件中，为更新时间添加前缀文字"更新于："，提高可读性
    - 在 HomeView 组件中，将文章列表的时间显示从更新时间改为发布日期

--- src/components/PostContent.vue
+++ src/components/PostContent.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="text-zinc-400 text-sm mb-6 flex items-center">
       <span class="h-4 w-0.5 rounded-full bg-zinc-200 mr-3"></span>
-      {{ new Date(post.updated).toLocaleString() }}
+      更新于：{{ new Date(post.updated).toLocaleString() }}
     </div>
     <h1 class="text-2xl sm:text-3xl font-normal mb-9">{{ post.title }}</h1>
     <div
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <ul>
     <li v-for="post in posts" :key="post.slug" class="py-7 border-b border-gray-200 relative">
       <div class="text-zinc-400 text-xs mb-2 sm:mb-0 sm:absolute sm:left-0 sm:top-8.5">
-        {{ new Date(post.updated).toLocaleDateString() }}
+        {{ new Date(post.published).toLocaleDateString() }}
       </div>
       <h2 class="hover:text-sky-500 font-light sm:text-xl transition duration-300 sm:ml-24">
         <RouterLink :to="`/detail/${post.slug}`">

89e2f621cab2004c730340e2c0bd8a28a9a130e7
Quincy <quincy@games.com>
2025-06-27 16:11:31 +0800

    feat: 在配置文件中添加自定义字体样式链接

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/@lobehub/webfont-harmony-sans-sc@1.0.0/css/index.min.css",
+        },
+      ],
+    },
+  },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@unocss/nuxt", "nuxt-shiki"],
